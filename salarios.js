58842174f7854903365b4a9cf36ca14d16d223a5
LaloMC95 <eduardomc134@gmail.com>
2021-10-05 16:30:32 -0500

    Updated

---
+++ salarios.js
@@ -0,0 +1,30 @@
+const colombia = [];
+colombia.push({
+    name: "Eduardo",
+    salary: 500,
+});
+
+colombia.push({
+    name: "Daniel",
+    salary: 1000,
+});
+
+colombia.push({
+    name: "Valeria",
+    salary: 15000,
+});
+
+colombia.push({
+    name: "Omar",
+    salary: 1600,
+});
+
+colombia.push({
+    name: "Lidia",
+    salary: 1800,
+});
+
+colombia.push({
+    name: "Candy",
+    salary: 2000,
+});
